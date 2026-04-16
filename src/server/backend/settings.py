0f6741a108c057040f38648c334dd0c870867ade
Nils Logins <nils.logins@aerones.com>
2026-04-16 20:42:25 +0300

    fix: add amonzi.com to CORS allowed origins

--- src/server/backend/settings.py
+++ src/server/backend/settings.py
@@ -89,6 +89,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://amonzi.com",
+    "https://www.amonzi.com",
 ]
 
 REST_FRAMEWORK = {
